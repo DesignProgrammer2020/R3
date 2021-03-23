b228581982987835d7032659f6bd7ecd11f96c75
mlmargaretli6@gmail.com <>
2021-03-22 22:12:45 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,87 @@
+let plastics = [];
+
+let animals = [
+  {name: "cardinal",
+    thing: "plastic bottles"},
+  {name: "pelican",
+    thing: "plastic cups"},
+  {name: "kangaroo",
+    thing: "kazoos"},
+  {name: "sea turtle",
+    thing: "straws"},
+  {name: "squirrel",
+    thing: "streamers"},
+  {name: "peacock",
+    thing: "plastic utensils"},
+  {name: "sea lion",
+    thing: "soda cans"},
+  {name: "shark",
+    thing: "styrofoam plates"}
+];
+
+let button;
 let randomIndex;
 let animating = false;
-let birds = [];
 let img;
 let imageCounter = 0;
 
 function preload() {
-  for (let i = 0; i <= 15; i++) {
-    birds[i] = loadImage(`assets/birds_$ {i}.JPG`)
+  for (let i = 0; i <=15; i++) {
+    plastics[i] = loadImage(`pictures/plastics_${i}.jpg`)
   }
 }
 
 function setup() {
-  createCanvas(600, 600);
-  background(200, 220, 240);
-  textSize(24);
+  createCanvas(500, 500);
+  background(140, 130, 80);
+  textSize(28);
+  textStyle(BOLD);
+  fill(0);
   imageMode(CENTER);
   frameRate(12);
-  text("Click me", 50, 575);
-  console.log(birds);
+  text("Plastics harm animals!", 100, 300);
+
+  //click the button
+  button = createButton("Click to randomize");
+
+  button.mousePressed(buttonPressed);
 }
 
 function draw() {
   if (animating == true) {
     clear();
-    image(birds[0], width * 0.5, height * 0.5);
-    if (imageCounter < trolls.length - 1) {
+
+    //images of plastics
+    imageCounter %= plastics.length;
+    image(plastics[imageCounter],width/2, height/2);
+    if (imageCounter < plastics.length - 1) {
       imageCounter++;
-      console.log(imageCounter);
     } else {
       imageCounter = 0;
     }
   }
 }
 
-function randomizer(){
+function randomizer() {
   animating = false;
-  if(birds[0]) {
-    background(200, 220, 240);
+  if (animals[0]) {
+
     clear();
-    randomIndex = int(random(birds.length));
-    text(birds[randomIndex].name, 300, 300);
-    birds.splice(randomIndex, 1);
-  }else{
-    background();
-    text("That's all!", 200, 200)
+    randomIndexAnimals = int(random(animals.length));
+    randomIndexPlastics = int(random(plastics.length));
+
+    image(plastics[randomIndexPlastics], width * 0.5, height * 0.5);
+    text(`The ${animals[randomIndexAnimals].name} dislikes this.`, width * 0.15, height*0.82);
+
+    animals.splice(randomIndexAnimals, 1);
+
+  } else {
+    background(80, 180, 80);
+    text("Don't litter!", 150, 300);
   }
 }
 
-function mousePressed() {
+function buttonPressed() {
   animating = true;
-  setTimeout(randomizer, 2000);
+  setTimeout(randomizer, 1000);
 }
